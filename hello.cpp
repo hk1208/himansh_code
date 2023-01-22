c289fbe9d021bc8e67925c56465487f456615f8e
unknown <himanshukumar8march@gmail.com>
2023-01-22 13:16:04 +0530

    first commit

---
+++ hello.cpp
@@ -0,0 +1,79 @@
+#include<bits/stdc++.h>
+using namespace std;
+ // array<int,3>arr;//contains 0
+int main()
+{
+     //array<int,3>arr;// it will contain garbage value
+    // array<int,5>arr={1,5,6};//{1,5,6,0,0}
+     //array<int,5>arr;
+     //arr.fill(10);
+    // arr.at(index);
+    
+    // array<int,5>arr={1,2,3,4,5};
+     //for(auto it=arr.begin();it!=arr.end();it++)
+     //{
+     // cout<<*it<<" ";
+   /*  vector<int>arr;
+     cout<<arr.size()<<endl;
+     arr.push_back(0);
+     arr.push_back(2);
+
+     arr.clear();
+     vector<int>vec1(3,0);
+     vector<int>vec2(4,10);
+     vector<int>vec3(vec1.begin(),vec1.end());
+ 
+ for(auto it:vec3)
+ {
+   cout<<it<<" "<<endl;
+ }*/
+ /*
+ vector<vector<int>>vec;
+ vector<int>raj1;
+ raj1.push_back(1);
+ raj1.push_back(2);
+
+ vector<int>raj2;
+ raj2.push_back(10);
+ raj2.push_back(20);
+
+ vec.push_back(raj1);
+ vec.push_back(raj2);
+
+for(auto vctr: vec){
+  for(auto it : vctr)
+  {
+   cout<<it<<" ";
+  } 
+  cout<<endl;
+}
+vector<vector<int>>vectr(10,vector<int>(20,0));
+///vector<vector<vector<int>>>vec(10,vector<vector<int>>vectrr(10,vector<int>(20,0)));   ????
+  */
+ //set<int> myset{ 1, 2, 3, 4, 5 };
+ 
+    // using begin() to print set
+   // for (auto it = myset.begin(); it !=
+     //                        myset.end(); ++it)
+       // cout << ' ' << *it;
+    //return 0;
+
+}
+
+    
+
+
+/*
+namespace raj{
+   int val=50;
+   int getval(){
+      return val*10;
+   }
+
+}
+int main()
+{
+double val=10.0;
+cout<<val<<endl;
+cout<<raj::getval()<<endl;  
+}*/
